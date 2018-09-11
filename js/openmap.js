09f860cdce641156a11541bf902ff8abfc2d39f5
Caio <caio@wisetelecom.com.br>
2018-09-11 19:27:19 -0300

    Realizado a criação do primeiro release do projeto com as opções de Limpar mapa, Add marker, Add Polyline, Add markers e bound.

--- js/openmap.js
+++ js/openmap.js
@@ -4,7 +4,7 @@
 class OpenMap{
 	
 	/**
-	 * Adiciona um marker na instancia de Mapa.
+	 * Adiciona um marker na instância de Mapa.
 	 * 
 	 * @param mapid
 	 * @param lat
@@ -13,28 +13,96 @@ class OpenMap{
 	 */
 	constructor(mapid, lat, lon, zoom){
      
-	  this.map 	= L.map(mapid).setView([lat, lon], zoom);
-	
-	  const url 	  = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-	  const config  = {
-			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-	  };
-		
-      L.tileLayer(url, config).addTo(this.map);
-    }
+		  const url 	= "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+		  const config  = {
+		        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		  };
+			
+		  this.markers  = [];
+		  this.polyline = {};
+		  
+		  this.initialPoint = [lat, lon];
+		  this.defaultView  = {initialPoint: this.initialPoint, zoom: zoom}
+		  
+		  this.map = L.map(mapid).setView(this.initialPoint, zoom);
+		 	
+	      L.tileLayer(url, config).addTo(this.map);
+	}
 	
 	/**
-	 * Adiciona um marker na instancia de Mapa.
+	 * Executa a limpeza do Mapa
+	 */
+	clearMap(){
+		
+		for(var i = 0; i < this.markers.length; i++){
+			this.markers[i].remove();
+		}
+		
+		if(this.polyline instanceof L.Polyline)
+			this.polyline.remove();
+		
+		this.markers  = [];
+		
+		this.map.setView(this.defaultView.initialPoint, this.defaultView.zoom);
+	}
+	
+	/**
+	 * Adiciona um marker na instância de Mapa.
 	 * 
 	 * @param lat
 	 * @param lon
-	 * @param htmlData
+	 * @param title
+	 * @param htmlTemplate
+	 * @param icon
+	 * @returns Marker
 	 */
-	addMarker(lat, lon, htmlData){
+	addMarker(lat, lon, title, htmlTemplate, icon){
 		
-		L.marker([lat, lon])
-		 .addTo(this.map)
-		 .bindPopup(htmlData)
-		 .openPopup();
+		var options = new Object();
+		
+		options.title =  title;
+		
+		if(icon !== undefined)
+			options.icon = L.icon({iconUrl: icon, iconSize: [32, 37]});
+				
+		let marker = L.marker([lat, lon], options)
+		 			  .addTo(this.map);
+
+		this.markers.push(marker);
+		
+		if(htmlTemplate !== undefined)
+			marker.bindPopup(htmlTemplate).openPopup();
+		
+		return marker;
+	}
+	
+	/**
+	 * Adiciona o Zoom no marker
+	 */
+	addZoom(marker){
+		this.map.setView(marker._latlng, 20);
+	}
+	
+	/**
+	 * Adiciona o Zoom no marker
+	 */
+	addBounds(){
+		this.map.fitBounds(L.latLngBounds(this.markers.map(marker => marker._latlng)));
+	}
+	
+	/**
+	 * Adiciona um Poup no Marker.
+	 * 
+	 * @returns Polyline
+	 */
+	addPolyline(){
+		
+		var latlngs = this.markers.map(marker => marker._latlng);
+		
+		this.polyline = L.polyline(latlngs, {color: 'red'}).addTo(this.map);
+		
+		this.map.fitBounds(this.polyline.getBounds());
+		
+		return this.polyline;
 	}
 }
